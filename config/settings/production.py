debcbb6b9d9cd458667b6674eee671b48cf34804
Eugen Marin <marvingoodboy@gmail.com>
2021-06-24 18:48:47 +0300

    Мелкие настройки по библиотекам

--- config/settings/production.py
+++ config/settings/production.py
@@ -144,6 +144,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/dashboard')
 
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
